78beda669ae6edce9779ecb598deadb3409edf68
trevanpwrd <demonshtern@gmail.com>
2017-12-19 23:49:10 +0300

    Create Board + items (img/name)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import { Main } from './main'
-import { Login } from './login'
+import { Partners } from "./partners";
+import styled from 'styled-components'
+
+const Screen = styled.div`
+    width: 1440px;
+    height: 900px;
+    
+    background-image: url("https://pp.userapi.com/c841323/v841323711/48cc7/N46MlAIBQEs.jpg");
+`;
 
 class App extends Component {
 
   constructor() {
-    super()
+    super();
 
     this.state = {
       image:null,
@@ -23,44 +31,44 @@ class App extends Component {
   }
 
   componentWillMount() {
-    let type = "dima"
-    let base_url = "http://172.20.10.3:8000"
+    let type = "dima";
+    let base_url = "http://172.20.10.3:8000";
 
     if (type === "dima")
-      this.setImage("https://lh4.googleusercontent.com/z3N0gwMXTYCtX5iEYH7TTijbylSDNCmUfmn70FhwUwOTugrOIM4PpUjVHpNSTJAlZSr7RCuMLJBdhEenOacR=w1440-h782")
+      this.setImage("https://lh4.googleusercontent.com/z3N0gwMXTYCtX5iEYH7TTijbylSDNCmUfmn70FhwUwOTugrOIM4PpUjVHpNSTJAlZSr7RCuMLJBdhEenOacR=w1440-h782");
     else
-      this.setImage("https://lh3.googleusercontent.com/mr1TV8e6Wb1wKVxYJoGicALHv0Z5dbrPjp23WMh6JFuCqt87gJai5CbBlWT9LyhXk4wf95f3qidG7GrnMOmD=w1440-h782")
+      this.setImage("https://lh3.googleusercontent.com/mr1TV8e6Wb1wKVxYJoGicALHv0Z5dbrPjp23WMh6JFuCqt87gJai5CbBlWT9LyhXk4wf95f3qidG7GrnMOmD=w1440-h782");
 
     fetch(base_url + '/abc/' + type)
       .then(res => {
-        console.log(res)
+        console.log(res);
         if (res.status === 200) {
           return res.json()
         }
       })
-      .then(abc => this.setState({ abc: abc }))
+      .then(abc => this.setState({ abc: abc }));
 
     fetch(base_url + '/items/' + type)
       .then(res => {
-        console.log(res)
+        console.log(res);
         if (res.status === 200) {
           return res.json()
         }
       })
-      .then(products => this.setState({ products: products }))
+      .then(products => this.setState({ products: products }));
 
     fetch(base_url + '/selling/' + type)
       .then(res => {
-        console.log(res)
+        console.log(res);
         if (res.status === 200) {
           return res.json()
         }
       })
-      .then(selling => this.setState({ selling: selling }))
+      .then(selling => this.setState({ selling: selling }));
 
     fetch(base_url + '/count_abc/' + type)
       .then(res => {
-        console.log(res)
+        console.log(res);
         if (res.status === 200) {
           return res.json()
         }
@@ -68,59 +76,49 @@ class App extends Component {
       .then(count_abc => this.setState({ count_abc: count_abc }))
   }
 
-  openTableProduct = () => this.setState({isOpenTableProduct: true})
-  closeTableProduct = () => this.setState({isOpenTableProduct: false})
+  setType = type => this.setState({setType: type});
 
-  openTableCheck = () => this.setState({isOpenTableCheck: true})
-  closeTableCheck = () => this.setState({isOpenTableCheck: false})
-
-  openGraph = () => this.setState({isOpenGraph: true})
-  closeGraph = () => this.setState({isOpenGraph: false})
-
-  openTableABC = () => this.setState({isOpenTableABC: true})
-  closeTableABC = () => this.setState({isOpenTableABC: false})
-
-  setType = type => this.setState({setType: type})
-
-  setImage = url => this.setState({image: url})
+  setImage = url => this.setState({image: url});
 
   render() {
     return (
-      <div>
+      <Screen>
         <Route
           path="/main"
           component={() => (
             <Main
-              image={this.state.image}
-              abc={this.state.abc}
-              count_abc={this.state.count_abc}
-              products={this.state.products}
-              selling={this.state.selling}
-              isOpenTableProduct={this.state.isOpenTableProduct}
-              isOpenTableCheck={this.state.isOpenTableCheck}
-              isOpenTableABC={this.state.isOpenTableABC}
-              isOpenGraph={this.state.isOpenGraph}
-              openTableProduct={this.openTableProduct}
-              openTableCheck={this.openTableCheck}
-              openGraph={this.openGraph}
-              openTableABC={this.openTableABC}
-              closeTableProduct={this.closeTableProduct}
-              closeTableCheck={this.closeTableCheck}
-              closeGraph={this.closeGraph}
-              closeTableABC={this.closeTableABC}
+
+
+              // image={this.state.image}
+              // abc={this.state.abc}
+              // count_abc={this.state.count_abc}
+              // products={this.state.products}
+              // selling={this.state.selling}
+              // isOpenTableProduct={this.state.isOpenTableProduct}
+              // isOpenTableCheck={this.state.isOpenTableCheck}
+              // isOpenTableABC={this.state.isOpenTableABC}
+              // isOpenGraph={this.state.isOpenGraph}
+              // openTableProduct={this.openTableProduct}
+              // openTableCheck={this.openTableCheck}
+              // openGraph={this.openGraph}
+              // openTableABC={this.openTableABC}
+              // closeTableProduct={this.closeTableProduct}
+              // closeTableCheck={this.closeTableCheck}
+              // closeGraph={this.closeGraph}
+              // closeTableABC={this.closeTableABC}
             />
           )}
         />
         <Route
-          path="/login"
+          path="/partners"
           component={() => (
-            <Login
+            <Partners
               type={this.state.type}
               setType={this.setType}
             />
           )}
         />
-      </div>
+      </Screen>
     )
   }
 }
---
+++ src/partners/Board.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+import ItemPartner from "./ItemPartner";
+
+const Container = styled.div`
+    height: 100%;
+    width: 50%;
+    
+    background-color: #f4f5f7;   
+    
+    margin-top: 10%; //DELETE
+    margin-left: 15%;
+    
+    border: 2px solid black;
+    border-radius: 10px;
+`;
+
+const ul = styled.div`
+    -moz-column-count: 2;
+    -webkit-column-count: 2;
+    column-count: 2;
+    
+     // flex-wrap: wrap;
+`;
+
+const Board = () => (
+    <Container>
+        <ul>
+            <ItemPartner/>
+            <ItemPartner/>
+            <ItemPartner/>
+
+            <ItemPartner/>
+            <ItemPartner/>
+            <ItemPartner/>
+
+            <ItemPartner/>
+            <ItemPartner/>
+            <ItemPartner/>
+        </ul>
+    </Container>
+);
+
+export default Board
