dab97267f7d65635db5a9e45209c92b2b430a3c8
trevanpwrd <demonshtern@gmail.com>
2017-12-20 00:43:24 +0300

    Add header to Partners + change bgcolor on Feed

--- src/main/News.js
+++ src/main/News.js
@@ -6,17 +6,19 @@ const Title = styled.p`
     text-align: left;
     margin-left: 5%;
     font-size:150%;
-`
+`;
 
 const Image = styled.img`
     width: 20%;
-`
+`;
 
 const ContainerNews = styled.div`
     border-style: solid;
     text-align: center;
     border-width: 2px;
-`
+    
+    background-color: white;
+`;
 
 const News = () => (
     <ContainerNews>
@@ -24,6 +26,6 @@ const News = () => (
         <Image src="https://static.wixstatic.com/media/a8f7f8_d6bbaf029f6541008f08a3ad130a4151.png/v1/fill/w_113,h_113,al_c,usm_0.66_1.00_0.01/a8f7f8_d6bbaf029f6541008f08a3ad130a4151.png"/>
         <p>Описание Описание Описание Описание Описание Описание</p>
     </ContainerNews>
-)
+);
 
 export default News
--- src/partners/Container.js
+++ src/partners/Container.js
@@ -1,9 +1,20 @@
 import React from 'react'
-//import styled from 'styled-components'
+import styled from 'styled-components'
 import Board from './Board'
+import Header from "../main/Header";
+
+const Screen = styled.div`
+  padding-left:10%;
+  
+  wight: 1440px;
+  height: 900px;
+`;
 
 const Container = ({}) => (
+    <Screen>
+        <Header/>
         <Board/>
+    </Screen>
 );
 
 export default Container
