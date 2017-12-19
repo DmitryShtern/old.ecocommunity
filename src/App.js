251000199abb5a080a6d590290de15151b648c8f
trevanpwrd <demonshtern@gmail.com>
2017-12-20 00:11:08 +0300

    Add screen's style

--- src/App.js
+++ src/App.js
@@ -2,54 +2,59 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import { Main } from './main'
 
+const Screen = styled.div`
+    width: 1440;
+    height: 900;
+`;
+
 class App extends Component {
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      image:null,
-      type: null,
-      products: [],
-      selling:[],
-      abc:[],
-      count_abc: [],
-      isOpenTableProduct: true,
-      isOpenTableCheck: false,
-      isOpenGraph: false,
-      isOpenTableABC: false,
+        // this.state = {
+        //   image:null,
+        //   type: null,
+        //   products: [],
+        //   selling:[],
+        //   abc:[],
+        //   count_abc: [],
+        //   isOpenTableProduct: true,
+        //   isOpenTableCheck: false,
+        //   isOpenGraph: false,
+        //   isOpenTableABC: false,
+        // }
     }
-  }
 
-  componentWillMount() {
-    // fetch(base_url + '/abc/' + type)
-    //   .then(res => {
-    //     console.log(res)
-    //     if (res.status === 200) {
-    //       return res.json()
-    //     }
-    //   })
-    //   .then(abc => this.setState({ abc: abc }))
-  }
+    componentWillMount() {
+        // fetch(base_url + '/abc/' + type)
+        //   .then(res => {
+        //     console.log(res)
+        //     if (res.status === 200) {
+        //       return res.json()
+        //     }
+        //   })
+        //   .then(abc => this.setState({ abc: abc }))
+    }
 
-  render() {
-    return (
-      <Screen>
-        <Route
-          path="/main"
-          component={() => (
-            <Main/>
-          )}
-        />
-        <Route
-          path="/partners"
-          component={() => (
-            null
-          )}
-        />
-      </Screen>
-    )
-  }
+    render() {
+        return (
+            <Screen>
+                <Route
+                    path="/main"
+                    component={() => (
+                        <Main/>
+                    )}
+                />
+                <Route
+                    path="/partners"
+                    component={() => (
+                        null
+                    )}
+                />
+            </Screen>
+        )
+    }
 }
 
 export default App
