2ad6693f51fd7c4381cfb3d3364fcefb0261849d
trevanpwrd <demonshtern@gmail.com>
2017-12-20 00:09:43 +0300

    Add screen's style

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import { Main } from './main'
 
+const Screen = styled.div`
+    width: 1440;
+    height: 900;
+`;
+
 class App extends Component {
 
   constructor() {
     super();
 
-    this.state = {
-      image:null,
-      type: null,
-      products: [],
-      selling:[],
-      abc:[],
-      count_abc: [],
-      isOpenTableProduct: true,
-      isOpenTableCheck: false,
-      isOpenGraph: false,
-      isOpenTableABC: false,
-    }
+    // this.state = {
+    //   image:null,
+    //   type: null,
+    //   products: [],
+    //   selling:[],
+    //   abc:[],
+    //   count_abc: [],
+    //   isOpenTableProduct: true,
+    //   isOpenTableCheck: false,
+    //   isOpenGraph: false,
+    //   isOpenTableABC: false,
+    // }
   }
 
   componentWillMount() {
