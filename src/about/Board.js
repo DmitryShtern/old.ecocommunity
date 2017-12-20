ffc9f48e518af26549ded60908e711915a0d329b
trevanpwrd <demonshtern@gmail.com>
2017-12-20 20:03:55 +0300

    Review 'about'

--- src/about/Board.js
+++ src/about/Board.js
@@ -42,29 +42,34 @@ const ICO = styled.img`
     margin-right: 30px;
 `;
 
-const Board = ({map}) => (
-    <Container>
-        <AboutUs>
-            <h3>Краснодарская краевая экологическая общественная организация "Экологическое содружество" осуществляет деятельность в сфере охраны окружающей среды на территории Белореченского района Краснодарского края.</h3>
-            <p>Мы находимся по адресу: Краснодарский край, г. Белореченск, ул. Интернациональная, д. 27</p>
-            <p>Телефон: +7 (988) 389 - 28 - 48</p>
-            <h3>Мы на карте:</h3>
-        </AboutUs>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Internatsional'naya+Ulitsa,+27,+Belorechensk,+Krasnodarskiy+kray,+352637/@44.7543752,39.872228,702m/data=!3m1!1e3!4m5!3m4!1s0x40f0c5f27904432d:0x90f4489b2cfe9c88!8m2!3d44.7543752!4d39.8744167">
-            <MapImg src={map}/>
-        </a>
-        <SocIcons>
-            <a href="https://vk.com/kkooeco" target="_blank" rel="noopener noreferrer" >
-                <ICO src="http://localhost:8000/img/socicons/vk.png" width="50px"/>
+const Board = ({map}) => {
+    return (
+        <Container>
+            <AboutUs>
+                <h3>Краснодарская краевая экологическая общественная организация "Экологическое содружество"
+                    осуществляет деятельность в сфере охраны окружающей среды на территории Белореченского района
+                    Краснодарского края.</h3>
+                <p>Мы находимся по адресу: Краснодарский край, г. Белореченск, ул. Интернациональная, д. 27</p>
+                <p>Телефон: +7 (988) 389 - 28 - 48</p>
+                <h3>Мы на карте:</h3>
+            </AboutUs>
+            <a target="_blank" rel="noopener noreferrer"
+               href="https://www.google.com/maps/place/Internatsional'naya+Ulitsa,+27,+Belorechensk,+Krasnodarskiy+kray,+352637/@44.7543752,39.872228,702m/data=!3m1!1e3!4m5!3m4!1s0x40f0c5f27904432d:0x90f4489b2cfe9c88!8m2!3d44.7543752!4d39.8744167">
+                <MapImg src={map}/>
             </a>
-            <a href="https://ok.ru/kkooeco" target="_blank" rel="noopener noreferrer" >
-                <ICO src="http://localhost:8000/img/socicons/ok.png" width="50px"/>
-            </a>
-            <a href="https://fb.com/kkooeco" target="_blank" rel="noopener noreferrer" >
-                <ICO src="http://localhost:8000/img/socicons/fb.png" width="50px"/>
-            </a>
-        </SocIcons>
-    </Container>
-);
+            <SocIcons>
+                <a href="https://vk.com/kkooeco" target="_blank" rel="noopener noreferrer">
+                    <ICO src="http://localhost:8000/img/socicons/vk.png" width="50px"/>
+                </a>
+                <a href="https://ok.ru/kkooeco" target="_blank" rel="noopener noreferrer">
+                    <ICO src="http://localhost:8000/img/socicons/ok.png" width="50px"/>
+                </a>
+                <a href="https://fb.com/kkooeco" target="_blank" rel="noopener noreferrer">
+                    <ICO src="http://localhost:8000/img/socicons/fb.png" width="50px"/>
+                </a>
+            </SocIcons>
+        </Container>
+    );
+};
 
 export default Board
